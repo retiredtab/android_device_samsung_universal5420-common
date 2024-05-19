c673edc1a0d6f9591ccff65672977f03e3a0a9a7
Danny Trunk <dtrunk90@gmail.com>
2024-05-18 19:37:48 -0600

    universal5420-common: Remove deprecated TARGET_USES_64_BIT_BINDER

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -30,7 +30,7 @@ USE_XML_AUDIO_POLICY_CONF := 1
 BACKLIGHT_PATH := "/sys/class/backlight/panel/brightness"
 
 # Binder
-TARGET_USES_64_BIT_BINDER := true
+# TARGET_USES_64_BIT_BINDER := true
 
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
